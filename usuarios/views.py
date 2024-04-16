d8acbd41d3bab710c0fc1e865ed629b085a5f505
Claudinei Braga de Oliveira <oliveiraclaudinei786@gmail.com>
2024-04-16 16:58:27 -0300

    Validação de usuarios e adicinar imagem

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'cadastro.html')
-   
+    if request.method == "GET":
+        return render(request, 'cadastro.html')        
+    elif request.method == "POST":
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        senha=request.POST.get('senha')
+        confirmar_senha=request.POST.get('confirmar_senha')
+
+        if senha != confirmar_senha:
+            print("ERRO 2: O campo senha e confirme senha não são iguais")
+            return redirect('/usuarios/cadastro')
+        
+        if len(senha) < 6:
+            print('ERRO 3: Senha precisa de no minimo 6 caracteres')
+            return redirect('/usuarios/cadastro')
+        
+        users= User.objects.filter(username=username)
+        print(users.exists())
+
+        if users.exists():
+            print("ERRO 1 Usuario ja existe")
+            return redirect('/usuarios/cadastro')
+        
+        user=User.objects.create_user(
+            username=username,
+            email=email,
+            password=senha
+        )
+        
+        return HttpResponse(f'Usuario criado com sucesso')
+    
