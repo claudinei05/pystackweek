b585971090f3bc47eea57e56edc413f1ad628f8d
Claudinei Braga de Oliveira <oliveiraclaudinei786@gmail.com>
2024-04-18 17:47:38 -0300

    Add fitros por nome e por especialidades

--- paciente/views.py
+++ paciente/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from medico.models import DadosMedico
+from medico.models import DadosMedico, Especialidade
 
 # Create your views here.
 def home(request):
     if request.method == "GET":
+        medico_filtrar= request.GET.get('medico')
+        especialidade_filtrar = request.GET.getlist('especialidades')
         medicos = DadosMedico.objects.all()
-        return render(request, 'home.html', {'medicos':medicos})
+
+        if medico_filtrar:
+            medicos=medicos.filter(nome__icontains=medico_filtrar)
+        
+        if especialidade_filtrar:
+            medicos= medicos.filter(especialidade_id__in=especialidade_filtrar)
+
+        especialidades= Especialidade.objects.all()
+        return render(request, 'home.html', {'medicos':medicos, 'especialidades':especialidades})
