79e4c99290c1dd2f89f60225c45daff4cbb2996f
Claudinei Braga de Oliveira <oliveiraclaudinei786@gmail.com>
2024-04-18 19:08:18 -0300

    consultas

--- paciente/views.py
+++ paciente/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from medico.models import DadosMedico, Especialidade
+from datetime import datetime
+from django.shortcuts import redirect, render
+from medico.models import DadosMedico, Especialidade, DatasAbertas
+from .models import Consulta
+from django.contrib import messages
+from django.contrib.messages import constants
 
 # Create your views here.
 def home(request):
@@ -16,3 +20,29 @@ def home(request):
 
         especialidades= Especialidade.objects.all()
         return render(request, 'home.html', {'medicos':medicos, 'especialidades':especialidades})
+    
+    
+def escolher_horario(request, id_dados_medicos):
+    if request.method == "GET":
+        medico = DadosMedico.objects.get(id=id_dados_medicos)
+        datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now()).filter(agendado=False)
+        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+
+def agendar_horario(request, id_data_aberta):
+    if request.method == "GET":
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+
+        horario_agendado = Consulta(
+            paciente=request.user,
+            data_aberta=data_aberta
+        )
+
+        horario_agendado.save()
+
+        
+        data_aberta.agendado = True
+        data_aberta.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Horário agendado com sucesso.')
+
+        return redirect('/pacientes/minhas_consultas/')
